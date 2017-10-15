a2dcffc5367d0e471ea1eb123614fd3ae0048d44
Nadine van den Berg <nadine-vdberg@hotmail.com>
2017-10-15 15:51:56 +0200

    Democratische Drinkmeter

--- DemocratischeDorstmeter_v1/src/ofApp.cpp
+++ DemocratischeDorstmeter_v1/src/ofApp.cpp
@@ -1,4 +1,7 @@
 #include "ofApp.h"
+#define PIN_BUTTON1 12
+#define PIN_BUTTON2 9
+#include <glut.h>
 
 //--------------------------------------------------------------
 void ofApp::setup() {
@@ -6,6 +9,7 @@ void ofApp::setup() {
 	ofAddListener(arduino.EInitialized, this, &ofApp::setupArduino);		//Luister naar opstartsignaal van de Arduino
 	arduino.connect("COM3"); //maak verbinding met de arduino die aan deze poort verbonden zit
 	arduino.sendFirmwareVersionRequest();
+	ofLog() << "Boolean button 1 status: " << b1Pressed << endl;
 }
 
 //--------------------------------------------------------------
@@ -15,11 +19,14 @@ void ofApp::update() {
 
 //--------------------------------------------------------------
 void ofApp::draw() {
-
+	
 }
 
 void ofApp::setupArduino(const int & version)
 {
+	//verwijder listener
+	ofRemoveListener(arduino.EInitialized, this, &ofApp::setupArduino);
+
 	//print firmware details
 	ofLog() << "Arduino firmware found " << arduino.getFirmwareName() << arduino.getMajorFirmwareVersion() << arduino.getMinorFirmwareVersion() << endl;
 
@@ -34,11 +41,47 @@ void ofApp::setupArduino(const int & version)
 
 void ofApp::analogPinChanged(const int & pin)
 {
-	ofLog() << "Analog pin" << pin << "value: " << arduino.getAnalog(pin) << endl;
+
+
+	// 	ofLog() << "Analog pin" << pin << "value: " << arduino.getAnalog(pin) << endl;
 }
 
 void ofApp::digitalPinChanged(const int & pin)
 {
-	ofLog() << "Digital pin" << pin << "value: " << arduino.getDigital(pin) << endl;
+	if (arduino.getDigital(12)) {
+
+		if (b1Pressed) {
+			b1Pressed = false;
+			ofLog() << "Boolean button 1 status: " << b1Pressed << endl;
+			glutTimerFunc(3000, timerCallBack, 1);
+		}
+
+		if (studentenMetDorst < studenten) {
+			studentenMetDorst = studentenMetDorst + 1;
+			ofLog() << "Op dit moment hebben " << studentenMetDorst << " studenten dorst" << endl;
+
+		}
+		if (studentenMetDorst == 10) {
+			ofLog() << "Alle studenten hebben dorst! Tijd voor pauze?" << endl;
+		}
+
+	}
+
+	/*	if (arduino.getDigital(9)) {
+
+			if (studentenMetDorst < studenten / 2) {
+				ofLog() << "Minder dan de helft van alle studenten heeft dorst. De pauze zal moeten wachten" << endl;
+			}
+			if (studentenMetDorst >= studenten / 2) {
+				ofLog() << "De pauze is geactiveerd!" << endl;
+				studentenMetDorst = 0;
+			}
+		}
+		*/
+}
+
+void ofApp::timerCallBack(int status)
+{
+
 }
 
--- DemocratischeDorstmeter_v1/src/ofApp.h
+++ DemocratischeDorstmeter_v1/src/ofApp.h
@@ -14,5 +14,9 @@ private:
 	void setupArduino(const int& version);
 	void analogPinChanged(const int & pin);
 	void digitalPinChanged(const int & pin);
-		
+	int studentenMetDorst;
+	int studenten = 10;
+
+	bool b1Pressed = true;
+	void timerCallBack(int status);
 };
